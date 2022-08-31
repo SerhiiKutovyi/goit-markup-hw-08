4fde318a5f88a08c799aaf2262bc3f3edaef0a11
SerhiiKutovyi <wagenrex@gmail.com>
2022-08-31 14:58:21 +0300

    ДЗ-8 На удачу

--- js/mobil-menu.js
+++ js/mobil-menu.js
@@ -1,13 +1,13 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-  const mobileBtnClose = document.querySelector('[data-menu-close]');
+  const menuBtnRef = document.querySelector('.menu-open');
+  const mobileMenuRef = document.querySelector('.mobile-menu');
+  const mobileBtnClose = document.querySelector('.mobile-menu__close');
 
   menuBtnRef.addEventListener('click', () => {
-    mobileMenuRef.classList.toggle('is-open');
+    mobileMenuRef.classList.toggle('is-hidden');
   });
 
   mobileBtnClose.addEventListener('click', () => {
-    mobileMenuRef.classList.toggle('is-open');
+    mobileMenuRef.classList.toggle('is-hidden');
   });
 })();
